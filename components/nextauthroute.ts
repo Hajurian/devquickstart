104c5b6ac77addb59f3835c442a202b410f27485
Hajurian <julianmoet1@gmail.com>
2025-05-26 17:43:37 -0400

    added signin event to when user signs in

--- components/nextauthroute.ts
+++ components/nextauthroute.ts
@@ -1,12 +1,20 @@
+import { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
-const authOptions = {
+const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENTID || "",
       clientSecret: process.env.GOOGLE_SECRET || "",
     }),
   ],
+  events: {
+    async signIn({ user }) {
+      const email = user.email;
+
+      //Do some kind of database call with the user email here.
+    },
+  },
   pages: {
     signIn: "/signin",
   },
